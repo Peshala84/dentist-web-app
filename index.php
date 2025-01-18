df933bd9f4fff69698dbed21eba10a59c401fd65
Ramesh Peshala <rameshpeshala84@gmail.com>
2025-01-18 23:23:16 +0530

    Initialized dentist web repository

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dentist</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <header class="header fixed-top">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <a href="#home" class="logo">dental <span>Care.</span></a>
+                <nav class="nav">
+                    <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#services">Services</a>
+                    <a href="#reviews">Reviews</a>
+                    <a href="#contact">Contact</a>
+                </nav>
+                <a href="#contact" class="link-btn">make appointment</a>
+                <div id="menu-btn" class="fas fa-bars"></div>
+            </div>
+        </div>
+    </header>
+
+    <section class="home" id="home">
+        <div class="container">
+            <div class="row min-vh-100 align-items-center">
+                <div class="content">
+                    <h3>let us brighten your smile</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores animi a
+                        ccusamus officiis voluptate</p>
+                    <a href="#contact" class="link-btn">make appointment</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
